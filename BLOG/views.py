56817d5f44badc5fad0ddb71cb7bcb5fd1a13a24
andreallive <andreallive@hotmail.es>
2017-09-02 16:58:59 -0500

    muestra post

--- BLOG/views.py
+++ BLOG/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.utils import timezone
+from .models import Post
+def post_list(request):
+    posts=Post.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_creaciacion')
+    return render(request, 'blog/post_list.html',{'posts': posts})
